a5e0ca528ca60e402947c6d86e4252cff9b58169
Linus Sellberg <sellberg@gmail.com>
2021-04-05 17:01:42 +0200

    Socket#accept: Without eagain we don't need the loop.

--- src/nested_scheduler/io_uring_context.cr
+++ src/nested_scheduler/io_uring_context.cr
@@ -47,20 +47,18 @@ module NestedScheduler
     end
 
     def accept(socket, fiber, timeout)
-      loop do
-        # TODO: Timeout..
-        # TODO: Error handling if ring is full.
-        ring.sqe.accept(socket, user_data: fiber.object_id)
-        ring_wait do |cqe|
-          if cqe.success?
-            return cqe.res
-          elsif socket.closed?
-            return nil
-            #   elsif cqe.eagain?
-            #     next
-          else
-            raise Socket::ConnectError.from_errno("accept")
-          end
+      # TODO: Timeout..
+      # TODO: Error handling if ring is full.
+      ring.sqe.accept(socket, user_data: fiber.object_id)
+      ring_wait do |cqe|
+        if cqe.success?
+          return cqe.res
+        elsif socket.closed?
+          return nil
+          #   elsif cqe.eagain?
+          #     next
+        else
+          raise Socket::ConnectError.from_errno("accept", cqe.cqe_errno)
         end
       end
     end
