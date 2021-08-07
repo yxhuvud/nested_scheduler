e14ee113c7dbc8c6b399fefa7f1ae5163da5ee39
Linus Sellberg <sellberg@gmail.com>
2021-08-07 18:32:15 +0200

    Socket: Update monkeypatches to 1.1.1.

--- src/nested_scheduler/monkeypatch/socket.cr
+++ src/nested_scheduler/monkeypatch/socket.cr
@@ -1,21 +1,21 @@
 require "socket"
 
-class Socket < IO
+module Crystal::System::Socket
   include NestedScheduler::IOContext::IO
 
-  def accept_impl
+  def system_accept
     io, scheduler = context
     io.accept(self, scheduler, @read_timeout)
   end
 
-  def send(message) : Int32
+  def system_send(bytes : Bytes) : Int32
     io, scheduler = context
-    io.send(self, scheduler, message.to_slice, "Error sending datagram")
+    io.send(self, scheduler, bytes, "Error sending datagram")
   end
 
-  def send(message, to addr : Address) : Int32
+  def system_send_to(bytes : Bytes, addr : ::Socket::Address) : Int32
     io, scheduler = context
-    io.send(self, scheduler, message, addr)
+    io.send_to(self, scheduler, bytes, addr)
   end
 
   private def unbuffered_read(slice : Bytes)
@@ -28,22 +28,24 @@ class Socket < IO
     io.socket_write(self, scheduler, slice, "Error writing to socket")
   end
 
-  protected def recvfrom(slice)
+  protected def system_receive(slice)
     io, scheduler = context
     # we will see if these will have to be moved into the context
     sockaddr = Pointer(LibC::SockaddrStorage).malloc.as(LibC::Sockaddr*)
-    addrlen = LibC::SocklenT.new(sizeof(LibC::SockaddrStorage))
     # initialize sockaddr with the initialized family of the socket
     copy = sockaddr.value
     copy.sa_family = family
     sockaddr.value = copy
+
+    addrlen = LibC::SocklenT.new(sizeof(LibC::SockaddrStorage))
 
     bytes_read = io.recvfrom(self, scheduler, slice, sockaddr, addrlen, "Error receiving datagram")
 
     {bytes_read, sockaddr, addrlen}
   end
 
-  def connect(addr, timeout = nil)
+  def system_connect(addr, timeout = nil)
+    timeout = timeout.seconds unless timeout.is_a? ::Time::Span | Nil
     io, scheduler = context
 
     io.connect(self, scheduler, addr, timeout) do |error|
@@ -51,7 +53,13 @@ class Socket < IO
     end
   end
 
+  # def system_bind
+  # def system_listen
+  # def system_close_read
+  # def system_close_write
+  # def system_reuse_port?
+  # def system_reuse_port=
+  # def system_close
   # private def shutdown(how) # doesnt
   # private def unbuffered_close # for line that do libc.close x_x
-  # evented_close
 end
